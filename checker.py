1b8b2576f19fc564bea3e79f6907a7fbb0672ee5
Peter Barilo <102885083+PeterBarilo@users.noreply.github.com>
2022-10-01 14:43:28 -0500

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,15 +1,41 @@
-import PyPDF2
+import requests
+import hashlib
 import sys
 
-inputs = sys.argv[1:]
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching : {res.status_code}, check the API and try again')
+    return res
 
 
-def combine(pdf_list):
-    merger = PyPDF2.PdfFileMerger()
-    for pdf in pdf_list:
-        print(pdf)
-        merger.append(pdf)
-    merger.write('super.pdf')
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
 
-combine(inputs)
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times')
+        else:
+            print(f'{password} was not found')
+    return 'done'
+
+
+main(sys.argv[1:])
+
+pwned_api_check('123')
